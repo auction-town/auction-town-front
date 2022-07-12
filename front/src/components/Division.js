c44f181ddcde4b98a2aeb04749fbb6f15e4a18c3
3pointdev <motors3996@gmail.com>
2022-07-13 01:41:58 +0900

    add: N빵계산기의 타이틀을 만듬

--- front/src/components/Division.js
+++ front/src/components/Division.js
@@ -1,6 +1,9 @@
 export default function Division() {
 
   return <article className="article">
-    
+    <div className="division-title">
+      <h1>친구랑 N빵 계산하기</h1>
+      <p>오늘 친구와 사용한 금액 계산하기</p>
+    </div>
   </article>
 }
