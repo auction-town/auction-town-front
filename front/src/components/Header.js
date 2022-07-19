8163bf0c443f906e98c27cf19a3885e3d06d9d9d
3pointdev <motors3996@gmail.com>
2022-07-19 22:13:28 +0900

    add: 로그인 비로그인 UI 변경사항 적용

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,13 +1,25 @@
+import React,{useState} from 'react';
 import Logo from '../data/Logo.png'
 
 export default function Header(props) {
+  let login = null;
+  const [loginState,setLoginState] = useState("logined");
+
+  if(loginState === "unlogin"){
+    login = <a href='/' className="login" onClick={(event)=>{
+    event.preventDefault();
+    props.onChange(event.target.className);
+  }}>로그인</a>
+  } else if(loginState === "logined"){
+    login = <a href='/' className="login" onClick={(event)=>{
+      event.preventDefault();
+      props.onChange(event.target.className);
+    }}>로그아웃</a>
+  }
 
   return <header className="header">
     <a href='/' className="brand-name">모여모여</a>
     <a href='/'><img src={Logo} className="logo" alt='logo'/></a>
-    <a href='/' className="login" onClick={(event)=>{
-      event.preventDefault();
-      props.onChange(event.target.className);
-    }}>로그인</a>
+    {login}
   </header>
 }
